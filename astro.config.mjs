c3c4f87fb37134513e8de216c12825e68d2e0456
Hasse Bylov <hasse@bylov.com>
2025-08-27 12:38:40 +0200

    Fix variables for public website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://bylov.github.io/bylov.com',  // your repo path if not root
-  base: '/bylov.com/',                        // matches repo name
+  site: 'https://bylov.com',
+  base: '/',  
   integrations: [tailwind({ applyBaseStyles: true })],
   output: 'static',
 });
